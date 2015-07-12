2b6232c7f614bd09bf2611ccdde503ffbae4fad1
Akshay Kher <akshaykher91@gmail.com>
2015-07-12 17:14:35 +0530

    Update LDA_comparison.R

--- LDA_comparison.R
+++ LDA_comparison.R
@@ -44,52 +44,6 @@ for(j in seq_len(nrow(train)))
 
 train$LDA = ifelse(train$LDA_03 > train$LDA_00 & train$LDA_01 & train$LDA_02 & train$LDA_04,1,0)
 
-for(j in seq_len(nrow(test)))
-{
-  if(j==1)
-  {
-    a=0
-    b=0
-    c=0
-    d=0
-    e=0
-    LDA_00 = vector()
-    LDA_01 = vector()
-    LDA_02 = vector()
-    LDA_03 = vector()
-    LDA_04 = vector()
-  }
-  x = c(train$LDA_00[j],train$LDA_01[j],train$LDA_02[j],train$LDA_03[j],train$LDA_04[j])
-  max = which.max(x)
-  if(max==1)
-  {
-    a=a+1
-    LDA_00[a] = j
-  }
-  if(max==2)
-  {
-    b=b+1
-    LDA_01[b] = j
-  }
-  if(max==3)
-  {
-    c=c+1
-    LDA_02[c] = j
-  }
-  if(max==4)
-  {
-    d=d+1
-    LDA_03[d] = j
-  }
-  if(max==5)
-  {
-    e=e+1
-    LDA_04[e] = j
-  }
-}
-
-test$LDA = ifelse(test$LDA_03 > test$LDA_00 & test$LDA_01 & test$LDA_02 & test$LDA_04,1,0)
-
 #> sum(train$shares[LDA_00])/length(LDA_00)
 #[1] 2996.37
 #> sum(train$shares[LDA_01])/length(LDA_01)
